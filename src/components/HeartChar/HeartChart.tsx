407ec5736326619669e96d6db68616670ffa677c
gkdfo40 <gkdfo40@gmail.com>
2022-05-28 14:08:24 +0900

    feat: add Chart

--- src/components/HeartChar/HeartChart.tsx
+++ src/components/HeartChar/HeartChart.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { VictoryArea, VictoryChart } from 'victory';
+import { VictoryArea, VictoryAxis, VictoryChart, VictoryLabel } from 'victory';
 import HEARTDATA1 from '../../data/heartrate/heartrate_136_0226_____1_.json';
 import HEARTDATA2 from '../../data/heartrate/heartrate_136_0308_____1_.json';
 import HEARTDATA3 from '../../data/heartrate/heartrate_136_0419_____1_.json';
@@ -16,18 +15,66 @@ const data = [
   { x: new Date(2021, 5, 11), y: 6 },
 ];
 
-const HeartChart = () => {
-  const date = '2022-02-26 20:21:31';
-  const month = date.split('-', 2);
-  const heartArr = [...HEARTDATA1, ...HEARTDATA2, ...HEARTDATA3];
-  const monthFilterData = heartArr.filter((item) => item.crt_ymdt.split('-', 2)[1] === month[1]);
+const heartArr = [...HEARTDATA1];
 
+interface IHeartBeat {
+  x: string;
+  y: number;
+}
+const filterData = () => {
+  const result: IHeartBeat[] = [];
+  heartArr.map((_data) => {
+    result.push({ x: _data.crt_ymdt, y: _data.avg_beat });
+  });
+  return result.reverse();
+};
+
+const HeartChart = () => {
+  // const date = '2022-02-26 20:21:31';
+  // const month = date.split('-', 2);
+  // const heartArr = [...HEARTDATA1, ...HEARTDATA2, ...HEARTDATA3];
+  // const monthFilterData = heartArr.filter((item) => item.crt_ymdt.split('-', 2)[1] === month[1]);
+
+  const newData = filterData();
   return (
-    <VictoryChart width={400} height={400}>
+    <VictoryChart width={800} height={400} domain={{ y: [0.4, 1] }} style={{ background: { fill: 'black' } }}>
+      <VictoryLabel x={15} y={15} text='BPM' style={{ fill: 'orange' }} />
+      <VictoryAxis
+        dependentAxis
+        orientation='left'
+        offsetX={50}
+        tickValues={[0.4, 0.54, 0.7, 0.84, 1]}
+        style={{ tickLabels: { fill: 'white' } }}
+        tickFormat={(t) => `${t * 150}`}
+      />
+      <VictoryAxis
+        // scale='time'
+        style={{
+          ticks: {
+            size: ({ tick }) => {
+              // const tickSize = tick.getFullYear() % 5 === 0 ? 10 : 5;
+              console.log(tick);
+              const tickSize = 4;
+              return tickSize;
+            },
+            stroke: 'white',
+            strokeWidth: 1,
+          },
+          tickLabels: { fill: 'white' },
+        }}
+        tickFormat={(t, i) => {
+          if (i % 6 === 0) {
+            const stringDate = new Date(t);
+            return `${stringDate}`;
+          }
+          return '';
+        }}
+      />
       <VictoryArea
-        data={data}
+        data={newData}
+        y={(datum) => datum.y / 150}
         interpolation='monotoneX'
-        style={{ data: { fill: 'rgba(0,0,0,0.5)', stroke: 'orange', strokeWidth: 2 } }}
+        style={{ data: { fill: 'red', stroke: 'orange', strokeWidth: 2 } }}
       />
     </VictoryChart>
   );
