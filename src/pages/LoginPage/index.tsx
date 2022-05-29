a89cce7f29e8ba0ef70e46ea7145c053c08ef197
김보현 <kimbohyeon@gimbohyeon-ui-MacBookPro.local>
2022-05-30 05:10:04 +0900

    feat: 로그인 로직 추가

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,13 +1,34 @@
+import { FormEvent, useRef } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+import { authenticateUser } from '../../utils/authenticateUser';
+
 import styles from './LoginPage.module.scss';
 
+// id: moaadmin1
+// password: moaadmin1
+
 const LoginPage = () => {
+  const idRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { status, message } = authenticateUser(idRef.current!.value, passwordRef.current!.value);
+    console.log(status, message);
+    toast(message);
+  };
+
   return (
-    <form className={styles.form}>
-      <h1>백오피스</h1>
-      <input type='text' className={styles.id} placeholder='id' />
-      <input type='password' className={styles.password} placeholder='password' />
-      <button type='submit'>로그인</button>
-    </form>
+    <>
+      <Toaster />
+      <form className={styles.form} onSubmit={handleLogin}>
+        <h1>백오피스</h1>
+        <input type='text' placeholder='id' ref={idRef} />
+        <input type='password' placeholder='password' ref={passwordRef} />
+        <button type='submit'>로그인</button>
+      </form>
+    </>
   );
 };
 
