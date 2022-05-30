82c4b4f19027da573d614c138a9793d8056926b7
hyemin065 <p62517@gmail.com>
2022-05-30 18:16:25 +0900

    fix: route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
-import Page from '../pages/Page';
+import Pages from '../pages/Page';
 
 const RootRoute = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-        <Route path='' element={<Page />} />
+        <Route path='' element={<Pages />} />
       </Route>
     </Routes>
   );
