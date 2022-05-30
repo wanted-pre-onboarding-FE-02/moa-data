3810f3e33c3664fee9c8a9b17ee6e5d27efd151a
doki333 <duckky333@gmail.com>
2022-05-30 23:31:42 +0900

    feat: searchResult layout & header

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
-export { ReactComponent as SearchIcon } from './search.svg'
-export { ReactComponent as StarIcon } from './star.svg'
-export { ReactComponent as CloseIcon } from './close.svg'
+export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as StarIcon } from './star.svg';
+export { ReactComponent as CloseIcon } from './close.svg';
+export { ReactComponent as ProfileIcon } from './profile.svg';
